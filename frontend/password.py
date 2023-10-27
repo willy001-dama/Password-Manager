6924b9207462bd693df2eca0b5e3baa93c1a9ccf
Dama Michael Yohanna <get2dama11@gmail.com>
2023-10-27 15:22:26 +0100

    added more functionality

--- frontend/password.py
+++ frontend/password.py
@@ -1,118 +1,145 @@
 from PySide6.QtWidgets import QPushButton, QLabel, \
     QLineEdit, QComboBox, QFrame, QVBoxLayout, QHBoxLayout, \
-    QHeaderView, QTableWidget, QTableWidgetItem, QMessageBox, QDialog, QCheckBox
+    QHeaderView, QTableWidget, QTableWidgetItem, QMessageBox, QDialog, QCheckBox, QGridLayout
 
+from PySide6.QtCore import Qt
 from PySide6.QtGui import QIcon
 
 from draw_line import QHSeparationLine, QVSeparationLine
+from frontend import draw_line
 
 
 class PasswordList(QFrame):
     def __init__(self, database_handle):
         super(PasswordList, self).__init__()
         self.database_handle = database_handle
+        self.setStyleSheet("QFrame{background:white;}")
         main_layout = QVBoxLayout()
         menu_layout = QHBoxLayout()
-        session_label = QLabel("Current Session")
-        session_label.setStyleSheet("padding:4px;font-size:15px;")
 
-        # filter_input.currentTextChanged.connect("me")
-        term_label = QLabel("Current Term")
-        term_label.setStyleSheet("padding:4px;font-size:15px;")
-        update_button = QPushButton("+ Add New")
-        update_button.clicked.connect(self.update_term_callback)
-        update_button.setStyleSheet("padding:8px;border-radius:0px;"
-                                    "background:rgb(14, 180, 166);color:white;font-weight:bold;")
-        add_button = QPushButton("Change Password")
-        add_button.clicked.connect(self.new_session_callback)
-        add_button.setStyleSheet("padding:8px;border-radius:0px;"
-                                 "background:rgb(14, 180, 166);color:white;font-weight:bold;")
+        add_new = QPushButton("+ Add New")
+        add_new.clicked.connect(self.add_new_login)
+        add_new.setStyleSheet("padding:8px;border-radius:0px;"
+                              "background:rgba(41, 128, 140,1);color:white;font-weight:bold;")
+        change_pass = QPushButton("Change Password")
+        change_pass.clicked.connect(self.update_password)
+        change_pass.setStyleSheet("padding:8px;border-radius:0px;"
+                                  "background:rgba(41, 128, 140,1);color:white;font-weight:bold;")
 
         # menu_layout.addWidget(session_label)
         # menu_layout.addWidget(QVSeparationLine())
         # menu_layout.addWidget(term_label)
-        menu_layout.addWidget(update_button)
-        menu_layout.addWidget(add_button)
+        menu_layout.addWidget(add_new)
+        menu_layout.addWidget(change_pass)
         menu_layout.addStretch()
         # menu_layout.addWidget()
+        name_label = QLabel("NAME")
+        created_on = QLabel("CREATED ON")
 
-        table = QTableWidget()
-        table.setColumnCount(2)
-        table.setRowCount(6)
-        # table.setColumnWidth(0)
-        table.setHorizontalHeaderLabels(["List Of All Sessions", "Status"])
-        table.setStyleSheet("QTableWidget::item {border: 0px; padding: 5px;}")
-        # sessions = [["2019", "Passed", ],
-        #            ["2020", "Passed",],
-        #            ["2021", "Passed",],
-        #            ["2022", "Passed",],
-        #            ["2023", "Current",]]
-        table.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
-        start = 0
+        password = QGridLayout()
+        password.addWidget(name_label, 0, 0)
+        password.addWidget(created_on, 0, 1)
+
+        def btn_click():
+            print("hello")
+
+        for row in range(2, 10, 2):
+            frame = QVBoxLayout()
+            site_value = QLabel("google.com")
+            site_value.setStyleSheet("font-size:20px; font-weight:bold;")
+
+            username_value = QLabel("Dama Michael")
+            username_value.setStyleSheet("font-size:10px;")
+
+            frame.addWidget(site_value)
+            frame.addWidget(username_value)
+
+            password.addLayout(frame, row, 0)
+
+            password.addWidget(QLabel("Two Days Ago"), row, 1)
+            action_button = QPushButton("....")
+            action_button.setStyleSheet("border:none;font-size:20px;cursor:hand;")
+            action_button.setFixedWidth(40)
+            action_button.clicked.connect(btn_click)
+            password.addWidget(action_button, row, 2)
+            password.addWidget(draw_line.QHSeparationLine(), row + 1, 0, 1, 3, Qt.AlignLeft)
 
         main_layout.addLayout(menu_layout)
         main_layout.addWidget(QHSeparationLine())
-        main_layout.addWidget(table)
+        main_layout.addLayout(password)
+        main_layout.addStretch()
 
         self.setLayout(main_layout)
 
-    def new_session_callback(self):
-        app = NewSession(self, self.database_handle)
+    def add_new_login(self):
+        app = NewPassword(self, self.database_handle)
         app.open()
 
-    def update_term_callback(self):
-        win = UpdateTerm(self, self.database_handle)
+    def update_password(self):
+        win = UpdatePassword(self, self.database_handle)
         win.open()
 
     def update_student_callback(self):
         pass
 
 
-class NewSession(QDialog):
-    """Dialog window for adding new session"""
+class NewPassword(QDialog):
+    """Dialog window for adding new password"""
 
     def __init__(self, parent, database_handle):
-        super(NewSession, self).__init__(parent)
+        super(NewPassword, self).__init__(parent)
         self.database_handle = database_handle
         self.setFixedWidth(320)
-        self.setWindowTitle("New Session")
+        self.setWindowTitle("New Password")
         self.setStyleSheet("QDialog{background:white;}")
         # create widget and layout
         layout = QVBoxLayout()
-        self.session_entry = QLineEdit()
-        self.session_entry.setObjectName('entry')
-        self.session_entry.setPlaceholderText("Enter session")
-        submit_btn = QPushButton("Submit")
-        submit_btn.setObjectName("submit")
-        submit_btn.clicked.connect(self.add_session)
+        self.sitename = QLineEdit()
+        self.sitename.setObjectName('entry')
+        self.sitename.setPlaceholderText("Enter Site Name")
+        self.username = QLineEdit()
+        self.username.setObjectName('entry')
+        self.username.setPlaceholderText("Enter Username")
+        self.password = QLineEdit()
+        self.password.setObjectName('entry')
+        self.password.setPlaceholderText("Enter Password")
+        submit_btn = QPushButton("Add Login")
+        submit_btn.setObjectName("Add Login")
+        submit_btn.clicked.connect(self.add_password)
         stat_frame = QHBoxLayout()
-        label = QLabel("Set As Current")
+        label = QLabel("Encrypt Password?")
         label.setStyleSheet("font-size:16px;padding-top:5px;")
-        self.is_current = QCheckBox()
+        self.encrypt_data = QCheckBox()
         stat_frame.addWidget(label)
-        stat_frame.addWidget(self.is_current)
-        layout.addWidget(self.session_entry)
+        stat_frame.addWidget(self.encrypt_data)
+        layout.addWidget(self.sitename)
+        layout.addWidget(self.username)
+        layout.addWidget(self.password)
+        # layout.addWidget(self.sitename)
+
         layout.addLayout(stat_frame)
         layout.addWidget(submit_btn)
         self.setLayout(layout)
 
-    def add_session(self):
-        session = self.session_entry.text()
-        current = self.is_current.isChecked()
-        if current:
-            sql = f"INSERT INTO Session (name, status) VALUES ('{session}', 'current');"
+    def add_password(self):
+        site_name = self.sitename.text()
+        username = self.username.text()
+        password = self.password.text()
+        encrypt = self.encrypt_data.isChecked()
+        if site_name and username and password:
+            self.database_handle.save_record(site_name, username, password, encrypt, owner=1)
+            QMessageBox.information(self, 'Success', "Logins Added sucessfully")
+
         else:
-            sql = f"INSERT INTO Session (name) VALUES ('{session}');"
-        self.database_handle.insert_record(sql)
-        QMessageBox.information(self, 'Success', "Session Added Successfully")
+            QMessageBox.information(self, 'Error Occur', "All fields most be filled!")
         self.destroy()
 
 
-class UpdateTerm(QDialog):
+class UpdatePassword(QDialog):
     """Dialog window for Updating current term"""
 
     def __init__(self, parent, database_handle):
-        super(UpdateTerm, self).__init__(parent)
+        super(UpdatePassword, self).__init__(parent)
         self.database_handle = database_handle
         self.setFixedWidth(320)
         self.setWindowTitle("Set Current Term")
